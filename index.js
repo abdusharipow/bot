c82ab4d4a93ad8b8566de03cf0bea2aaea4790bf
shoxrux <shoxruxabdusharipovv@gmail.com>
2025-11-03 17:53:08 +0500

    rasm qoshildi

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import TelegramBot from "node-telegram-bot-api";
 const TOKEN = "7851844004:AAGdHcrJpPKAYPdqlXIoDk9XqJhcJd1Odz4"
 
 const bot = new TelegramBot(TOKEN, { polling: true });
+const lamboPhotoURL = "./images/lamborghini_urus..webp"
 
-bot.on("message", function (msg) {
+bot.on("message", async function  (msg) {
   const chatId = msg.chat.id;
   const text = msg.text;
   const firstname = msg.chat.first_name;
@@ -19,7 +20,17 @@ bot.on("message", function (msg) {
       },
     });
   } else if (text == "Boshlash 🔥") {
-    bot.sendMessage(chatId, "Boshlanyaptii....");
+    const xabar = await  bot.sendMessage(chatId, "Iltimos kuting...");
+    setTimeout(function() {
+      bot.deleteMessage(chatId,xabar.message_id)
+    bot.sendPhoto(chatId, lamboPhotoURL, {
+      caption: `Lamborghini Urus** is a luxury SUV made by the Italian company Lamborghini. It was first introduced in 2018. The car has a 4.0-liter twin-turbo V8 engine that produces around 650 horsepower. It can go from 0 to 100 km/h in about 3.6 seconds and reach a top speed of 305 km/h. The Urus has an all-wheel-drive system and combines the power of a sports car with the comfort of an SUV. Its price starts at around $230,000.
+`
+    } )
+   
+    },1000)
+
+   
   } else if (text == "Menu 🥩") {
     bot.sendMessage(chatId, "Menyuga xush kelibsiz....");
   } else if (text == "Sozlamalar ⚙️") {
